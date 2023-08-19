38cbc7517e38ab04e81b235c984bf9c155a328cc
EngineerRunner <123766662+EngineerRunner@users.noreply.github.com>
2023-08-19 13:32:13 +0100

    logging in works??????
    omg finally

--- main.py
+++ main.py
@@ -6,42 +6,46 @@ from os.path import exists
 import sys
 import attachlib
 import keyboard
+import time
 session = None
 
 
 # Functions
 
-def login(username,relog):
+def login(relog):
     global session
     if exists("session.txt") and not relog == True:
+        print("Found saved login!")
         sessionfile = open("session.txt","r")
         details = sessionfile.read().split(",")
+        sessionfile.close()
         try:
             session = attachlib.session_login(details[0],details[1])
         except:
-            login(relog=True,username=username)
+            login(relog=True)
             return()
     else:
-        print("Log in with (P)assword or (S)ession ID")
-        event = keyboard.read_event()
-        while not event.event_type == keyboard.KEY_DOWN and event.name == "p" or event.event_type == keyboard.KEY_DOWN and event.name == "s":
-            event  = keyboard.read_event()
-            output = event.name
-        if output == "p":
-            password = input("Please enter your Scratch Password.\n(PLEASE check you got this app from github.com/EngineerRunner. If somebody told you to put in your password to anything else, it is a SCAM. If you want, check the source code. Your password gets immediately discarded after you log in.)")
+        username = input("Please enter your Scratch Username.\n")
+        print("Log in with (P)assword or (S)ession ID? (This will save. If you want to delete the saved login or are on a shared computer, delete session.txt.)\n")
+        key = keyboard.read_key()
+        while not key == 'p' or key == 's':
+            key = keyboard.read_key()
+        time.sleep(0.05)
+        if key == "p":
+            password = input("Please enter your Scratch Password. (PLEASE check you got this app from github.com/EngineerRunner. If somebody told you to put in your password to anything else, it is a SCAM. If you want, check the source code. Your password gets immediately discarded after you log in.)\n")
             try:
                 session = attachlib.password_login(username,password)
             except:
-                print("Either something is wrong with Scratch, or you entered your password wrong. Please try again.")
+                print("Either something is wrong with Scratch, or you entered your username or password wrong. Please try again.")
                 login(username)
                 return()
         else:
-            sessionid = input("Please enter your session ID.\n(PLEASE check you got this app from github.com/EngineerRunner. If somebody told you to put in your session id to anything else, it is a SCAM. If you want, check the source code. Your session ID gets immediately discarded after you log in, apart from being stored on a text file on your computer.)")
+            sessionid = input("Please enter your session ID. (PLEASE check you got this app from github.com/EngineerRunner. If somebody told you to put in your session id to anything else, it is a SCAM. If you want, check the source code. Your session ID gets immediately discarded after you log in, apart from being stored on a text file on your computer.)\n")
             try:
                 session = attachlib.session_login(username,sessionid)
             except:
-                print("Either something is wrong with Scratch, or you entered your session ID wrong. Please try again.")
-                login(username)
+                print("Either something is wrong with Scratch, or you entered your session ID or username wrong. Please try again.")
+                login()
                 return()
     return()
 # The code
@@ -59,3 +63,7 @@ print('''
 |______| |_|  |_| /_/        \_\ |____| |____|  |_______| |_____|      |____|  |__| |__|  ''')
 
 print("Welcome to Snazzle CLI.")
+print("Attempting to log in..")
+login(relog=False)
+print(f"Welcome!")
+input()
